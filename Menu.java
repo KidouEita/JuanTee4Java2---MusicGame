b132e9c0b1d5030621a3406ed51abe233f847000
KidouEita <h1537594628@gmail.com>
2016-06-06 02:25:24 +0800

    Add StateMents for Music
    It can play Music(but not able to linked to balls)
    And Add a Test Music

---
+++ Menu.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicgame;
+
+/**
+ *
+ * @author KidouEita
+ */
+
+import javafx.scene.layout.Pane;
+
+public class Menu extends Pane{
+}
